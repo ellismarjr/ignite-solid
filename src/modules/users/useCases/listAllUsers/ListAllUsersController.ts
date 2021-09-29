3b19733dfc586e504cf8186ebb12cac536f1d9c4
Junior de Oliveira <ellismarjr@hotmail.com>
2021-09-29 17:15:39 -0300

    fix: when occour erros

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,9 +8,14 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
     const userId = String(user_id);
-    const users = this.listAllUsersUseCase.execute({ user_id: userId });
 
-    return response.status(200).json(users);
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: userId });
+
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
